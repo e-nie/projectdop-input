ec004e9a55c09688b83ddf510dc433652758b183
Eva Niehaus <eniehaus111@gmail.com>
2023-06-30 20:11:48 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+
+import './App.css';
+
+function App() {
+  let [message, setMessage] = useState([
+    {message:'message1'},
+    {message:'message2'},
+    {message:'message3'},
+  ])
+  return (
+    <div className="App">
+     <div>
+       <input />
+       <button>+</button>
+     </div>
+      {message.map((el, index)=> {
+        return (
+            <div key={index}>{el.message}</div>
+        )
+      })}
+    </div>
+  );
+}
+
+export default App;
